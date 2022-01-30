1956503e9ceaf91ea7835c72792d0484ca94001d
ItzZed <bellzach8@gmail.com>
2022-01-29 21:21:51 -0700

    added jest

--- src/index.js
+++ src/index.js
@@ -9,16 +9,45 @@ import figlet from "figlet";
 import { createSpinner } from "nanospinner"
 
 // Import API
-import {API} from "./api/api.js";
+import { API } from "./api/api.js";
 
+// Api KEY HERE
+const key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-const api = new API();
+// start the api up
+const api = new API(key);
 
-api.auth('', '').then(() => {
-	// auth was successful, go make some requests!
+console.log(`${chalk.bgBlueBright("Welcome to VAL-CLI!")} The Most Broken CLI EVER LOL!`);
 
+try {
 
-}).catch((error) => {
-	console.log(error);
-});
+	api.getValContent();
 
+}
+catch(e) {
+
+	/*if(e.response.data.status.statusCode === 403) {
+
+		console.log(`${chalk.bgRed("F YOU RIOT GIVING ME 403 ERROR  GOD DAMINT")}`);
+		console.log(`${chalk.bgBlueBright("this probably means either api key is wrong or riot is fucking with me :)")}`);
+
+	}
+	else if(e.response.status === 401) {
+
+		console.log(`${chalk.bgRed("F YOU RIOT GIVING ME 401 ERROR  GOD DAMINT")}`);
+		console.log(`${chalk.bgGreenBright("shit i forgot the api key sorry riot")}`);
+
+	}
+	else {
+
+		console.log("unknown status error begins");
+		console.log(e);
+		console.log("unknown status error ends");
+
+	}*/
+
+	// why wont the above catch work? idk lol sorry for swears its like 1am
+	// brruh what the fuck is this code
+	console.log(e);
+
+}
