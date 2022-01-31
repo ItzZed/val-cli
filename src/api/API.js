766f69448e752297d801fee1eebbb3b537721b68
ItzZed <bellzach8@gmail.com>
2022-01-31 15:02:21 -0700

    Update API.js

--- src/api/API.js
+++ src/api/API.js
@@ -16,24 +16,9 @@ const userInfoUrl = "https://auth.riotgames.com/userinfo";
 const regionUrl = "https://riot-geo.pas.si.riotgames.com/pas/v1/product/valorant";
 const gameEntitlementUrl = "https://clientconfig.rpg.riotgames.com/api/v1/config/player";*/
 
-const ciphers = [
-	"TLS_AES_128_GCM_SHA256",
-	"TLS_AES_256_GCM_SHA384",
-	"TLS_CHACHA20_POLY1305_SHA256",
-	"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-	"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-	"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-	"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-	"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
-	"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
-	"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
-	"TLS_RSA_WITH_AES_128_GCM_SHA256",
-	"TLS_RSA_WITH_AES_256_GCM_SHA384",
-	"TLS_RSA_WITH_AES_128_CBC_SHA",
-	"TLS_RSA_WITH_AES_256_CBC_SHA"
-].join(':');
+// not posting ciphers sorry fam.
 
+import { ciphers } from "./ciphers.js";
 
 // Setup CookieJar
 const jar = new CookieJar();
